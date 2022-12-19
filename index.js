93745c48123e5658fb765927fdda3bcefcd46285
Rjrebel <rohitjibhakate111@gmail.com>
2022-12-19 18:10:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function factorialize(num) {
+  var result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
+}
+
+// Export to make the function available to other packages
+module.exports = factorialize;
